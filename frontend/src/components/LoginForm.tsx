b645aadb4368c049c46d5db610a98ee90cc2057b
Dhiraj Kuttichirayil <dhirajk1@gmail.com>
2022-09-30 22:02:17 -0500

    Integrating styles and making test cases

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import Image from 'next/image'
+import logo from "./cs_222_logo.jpeg";
+//Inspired by https://www.youtube.com/watch?v=7uKVFD_VMT8
+
 interface Details {
   name: string;
   email: string;
@@ -21,38 +25,50 @@ function LoginForm({ Login, error }: IProps) {
   };
 
   return (
-    <form onSubmit={(e) => submitHandler(e)}>
-      <div className="form-inner">
-        <h2>Login</h2>
-        {error != "" ? <div className="error">{error}</div> : ""}
-        <div className="form-group">
+  <div className="body">
+    <div className="wrapper" role="logo">
+    <div className="logo">
+      <Image className="img" src={logo} layout="intrinsic"/>
+    </div>
+  </div>
+  <div className="container">
+    <form className="form" role="form" id="login" onSubmit={(e) => submitHandler(e)}>
+        <h1 className="form__title">Login</h1>
+        {error != "" ? <div className="form__message--error">{error}</div> : ""}
+        <div className="form__input-group">
           <label htmlFor="name">Name:</label>
           <input
             type="text"
             name="name"
+            aria-label="name"
             id="name"
+            className="form__input"
             onChange={(e) => setDetails({ ...details, name: e.target.value })}
             value={details.name}
           />
         </div>
 
-        <div className="form-group">
+        <div className="form__input-group">
           <label htmlFor="email">Email:</label>
           <input
             type="text"
             name="email"
             id="email"
+            aria-label="email"
+            className="form__input"
             onChange={(e) => setDetails({ ...details, email: e.target.value })}
             value={details.email}
           />
         </div>
 
-        <div className="form-group">
+        <div className="form__input-group">
           <label htmlFor="password">Password:</label>
           <input
             type="password"
             name="password"
             id="password"
+            aria-label="password"
+            className="form__input"
             onChange={(e) =>
               setDetails({ ...details, password: e.target.value })
             }
@@ -60,9 +76,10 @@ function LoginForm({ Login, error }: IProps) {
           />
         </div>
 
-        <input type="submit" value="LOGIN" />
-      </div>
+        <input aria-label="button" className="form__button" type="submit" value="LOGIN" />
     </form>
+    </div>
+    </div>
   );
 }
 
