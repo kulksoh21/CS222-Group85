7464fac4c79fc7c3649ccfb028a1698dbe5d49c0
Dhiraj Kuttichirayil <dhirajk1@gmail.com>
2022-10-12 18:06:00 -0500

    Revert "Create Account Completed"

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -7,12 +7,6 @@ interface Details {
   name: string;
   email: string;
   password: string;
-  name1: string;
-  name2: string;
-  email1: string;
-  email2: string;
-  password1: string;
-  password2: string;
 }
 interface IProps {
   Login: (arg0: Details) => void;
@@ -20,15 +14,9 @@ interface IProps {
 }
 function LoginForm({ Login, error }: IProps) {
   const [details, setDetails] = useState<Details>({
-  name: "",
-  email: "",
-  password: "",
-  name1: "",
-  name2: "",
-  password1: "",
-  password2: "",
-  email1: "",
-  email2: "",
+    name: "",
+    email: "",
+    password: "",
   });
 
   const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
@@ -39,19 +27,15 @@ function LoginForm({ Login, error }: IProps) {
   <div className="body">
     <div className="wrapper" role="logo">
     <div className="logo">
-      <Image className="img" src={logo} layout="intrinsic" alt="Company Logo"
-       width="500" height="450"/>
+      <Image className="img" src={logo} layout="intrinsic" alt="Company Logo"/>
     </div>
   </div>
   <div className="container">
     <form className="form" role="form" id="login" onSubmit={(e) => submitHandler(e)}>
         <h1 className="form__title">Login</h1>
         {error != "" ? <div className="form__message--error">{error}</div> : ""}
-
-        {/* username input area when logging in */}
-
         <div className="form__input-group">
-          <label htmlFor="name">Username:</label>
+          <label htmlFor="name">Name:</label>
           <input
             type="text"
             name="name"
@@ -62,8 +46,6 @@ function LoginForm({ Login, error }: IProps) {
             value={details.name}
           />
         </div>
-
-        {/* email input area when logging in */}
 
         <div className="form__input-group">
           <label htmlFor="email">Email:</label>
@@ -77,8 +59,6 @@ function LoginForm({ Login, error }: IProps) {
             value={details.email}
           />
         </div>
-
-        {/* password input area when logging in */}
 
         <div className="form__input-group">
           <label htmlFor="password">Password:</label>
@@ -95,99 +75,9 @@ function LoginForm({ Login, error }: IProps) {
           />
         </div>
 
-       <input aria-label="button" className="form__button" type="submit" value="Login" />
-       <p><a href="http://www.google.com/">Don't have an Account? Create Account</a></p>
+        <input aria-label="button" className="form__button" type="submit" value="LOGIN" />
     </form>
     </div>
-
-    <div className="container1">
-    <form className="form" role="form" id="create_account" onSubmit={(e) => submitHandler(e)}></form>
-    <h1 className="form__title">Create Account</h1>
-        {error != "" ? <div className="form__message--error">{error}</div> : ""}
-
-        {/* username input area when creating account */}
-
-        <div className="form__input-group">
-          <label htmlFor="name">Username:</label>
-          <input
-            type="text"
-            name="name"
-            aria-label="name"
-            id="name"
-            // autofocus="username"
-            className="form__input"
-            onChange={(e) => setDetails({ ...details, name1: e.target.value })}
-            value={details.name1}
-          />
-        </div>
-
-        {/* email input area when creating account */}
-
-        <div className="form__input-group">
-          <label htmlFor="name">Email:</label>
-          <input
-            type="text"
-            name="name"
-            aria-label="name"
-            id="name"
-            // autofocus="email address"
-            className="form__input"
-            onChange={(e) => setDetails({ ...details, email1: e.target.value })}
-            value={details.email1}
-          />
-        </div>
-
-        {/* confirm email input area when creating account */}
-
-        <div className="form__input-group">
-          <label htmlFor="name">Confirm Email:</label>
-          <input
-            type="text"
-            name="name"
-            aria-label="name"
-            id="name"
-            // autofocus="email address"
-            className="form__input"
-            onChange={(e) => setDetails({ ...details, email2: e.target.value })}
-            value={details.email2}
-          />
-        </div>
-
-        {/* password input area when creating account */}
-
-        <div className="form__input-group">
-          <label htmlFor="name">Password:</label>
-          <input
-            type="text"
-            name="name"
-            aria-label="name"
-            id="name"
-            // autofocus="email address"
-            className="form__input"
-            onChange={(e) => setDetails({ ...details, password1: e.target.value })}
-            value={details.password1}
-          />
-        </div>
-
-        {/* confirm password input area when creating account */}
-
-        <div className="form__input-group">
-          <label htmlFor="name">Confirm Password:</label>
-          <input
-            type="text"
-            name="name"
-            aria-label="name"
-            id="name"
-            // autofocus="email address"
-            className="form__input"
-            onChange={(e) => setDetails({ ...details, password2: e.target.value })}
-            value={details.password2}
-          />
-
-          <input aria-label="button" className="form__button" type="submit" value="Create Account" />
-        </div>
-
-        </div>
     </div>
   );
 }
