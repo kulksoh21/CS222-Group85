0860770ee3e704e2d70cbf1d27ed68cb31f264b5
AryanMashruwala <amash2@illinois.edu>
2022-11-16 13:50:18 -0600

    SplitRemoveEdit/DeletedUnessecaryComments

--- src/pages/calendar.test.tsx
+++ src/pages/calendar.test.tsx
@@ -35,8 +35,6 @@ describe("Calendar Page Rendering",  () => {
     //check if pressing add event creates event on calendar
     const button = screen.getByRole("button", {name: /Add Event/i});
     fireEvent.click(button); 
-    //const event = screen.getByText("do work");
-    //expect(event).toBeInTheDocument();
   })
 
   it("Remove Event Works", () => {
